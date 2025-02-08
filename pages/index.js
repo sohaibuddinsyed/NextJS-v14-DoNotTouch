1b4c5da46dd83eae67faffd02d333eafac1ffdd9
Sohaib Uddin Syed <coolsohaib602@gmail.com>
2025-02-08 14:35:47 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,14 @@ function createLargeArray() {
   const arraySize = 2**28 // Large array size
   const largeArray = new Array(arraySize);
   const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
+   const memoryData = process.memoryUsage();
 
+    const memoryUsage = {
+      rss: `${formatMemoryUsage(memoryData.rss)} -> Resident Set Size - total memory allocated for the process execution`,
+      heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the allocated heap`,
+      heapUsed: `${formatMemoryUsage(memoryData.heapUsed)} -> actual memory used during the execution`,
+      external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
+    };
 
   try {
     for (let i = 0; i < arraySize; i++) {
